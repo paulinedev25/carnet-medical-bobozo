2fa1ef4b50f4da6949934d48f7d6ed0f490d75d8
paulinedev25 <paulinekaluila@gmail.com>
2025-12-27 17:38:34 +0100

    Fix paths services/api.js et build réussi pour déploiement

--- frontend/services/api.js
+++ frontend/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL, // doit pointer vers Render
+  baseURL: import.meta.env.VITE_API_URL,
   headers: { "Content-Type": "application/json" },
 });
 
