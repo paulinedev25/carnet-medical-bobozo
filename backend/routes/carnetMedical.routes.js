6dd8f2d7c3bbddbe720102552b596ea8dd03af2b
paulinedev25 <paulinekaluila@gmail.com>
2026-01-04 22:57:07 +0100

    Ajout auth.middleware pour déploiement

--- backend/routes/carnetMedical.routes.js
+++ backend/routes/carnetMedical.routes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 const CarnetMedicalController = require("../controllers/carnetMedical.controller");
-const authMiddleware = require("../middleware/auth.middleware");
+const authMiddleware = require("../middlewares/auth.middleware");
 
 router.get(
   "/:patientId",
